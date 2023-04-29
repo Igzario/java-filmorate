a32dbe96baa7b9a7b58f9698d51d33f9142b24b8
VOVA.4UMA <vkur34@gmail.com>
2023-04-29 22:35:57 +0300

    9sprint - 1st.3

--- src/main/java/ru/yandex/practicum/filmorate/message/AppMessageOk.java
+++ src/main/java/ru/yandex/practicum/filmorate/message/AppMessageOk.java
@@ -2,13 +2,12 @@ package ru.yandex.practicum.filmorate.message;
 import lombok.Getter;
 
 public class AppMessageOk extends AppMessageError{
-
     @Getter
     private Object object;
 
     public AppMessageOk(int statusCode, String message, Object obj) {
         this.setStatusCode(statusCode);
         this.setMessage(message);
-        this.object=obj;
+        this.object = obj;
     }
 }
