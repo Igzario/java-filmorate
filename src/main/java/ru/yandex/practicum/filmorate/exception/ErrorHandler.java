b08d07c0960816ef9dcf7a5ba77aed22a794cf05
VOVA.4UMA <vkur34@gmail.com>
2023-05-26 16:52:41 +0300

    10sprint - 2st

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.exception;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import java.util.Map;
 
 @Slf4j
@@ -11,25 +13,25 @@ import java.util.Map;
 public class ErrorHandler {
 
     @ExceptionHandler
-    public ResponseEntity userNotFound(final UserNotFoundExceptinon exception){
+    public ResponseEntity userNotFound(final UserNotFoundExceptinon exception) {
         log.error(exception.getMessage());
         return new ResponseEntity<>(Map.of("Error: ", exception.getMessage()), HttpStatus.valueOf(404));
     }
 
     @ExceptionHandler
-    public ResponseEntity filmNotFound(final FilmNotFoundExceptinon exception){
+    public ResponseEntity filmNotFound(final FilmNotFoundExceptinon exception) {
         log.error(exception.getMessage());
         return new ResponseEntity<>(Map.of("Error: ", exception.getMessage()), HttpStatus.valueOf(404));
     }
 
     @ExceptionHandler
-    public ResponseEntity validationException(final ValidationException exception){
+    public ResponseEntity validationException(final ValidationException exception) {
         log.error(exception.getMessage());
         return new ResponseEntity<>(Map.of("Error: ", exception.getMessage()), HttpStatus.valueOf(400));
     }
 
     @ExceptionHandler
-    public ResponseEntity runtimeException(final RuntimeException exception){
+    public ResponseEntity runtimeException(final RuntimeException exception) {
         log.error("Не предвиденная ошибка: " + exception.getMessage());
         return new ResponseEntity<>(Map.of("Не предвиденная ошибка: ", exception.getMessage()), HttpStatus.valueOf(500));
     }
