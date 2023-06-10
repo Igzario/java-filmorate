e7fa32a652983abfaddecf2ce4b540d0c14f4656
Igzario <88246005+Igzario@users.noreply.github.com>
2023-06-10 14:39:05 +0300

    database16

--- src/main/java/ru/yandex/practicum/filmorate/exception/SqlException.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-import java.sql.SQLException;
-
-public class SqlException extends SQLException {
-
-    public SqlException(String reason, String SQLState, int vendorCode) {
-        super("ОШибка SQL" + reason, SQLState, vendorCode);
-    }
-}
